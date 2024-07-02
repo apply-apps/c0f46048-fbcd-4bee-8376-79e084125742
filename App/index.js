f50a12c222b827165714b6a203c427f8c44f7655
Apply Magic <magic@goappy.app>
2024-07-02 16:57:41 +0000

    7/2/2024, 4:57:41 PM

--- App/index.js
+++ App/index.js
@@ -7,7 +7,7 @@ import { SafeAreaView, StyleSheet, Text } from 'react-native';
 export default function App() {
     return (
         <SafeAreaView style={styles.container}>
-            <Text style={styles.title}>Hello World</Text>
+            <Text style={styles.title}>Hello Wolrd</Text>
         </SafeAreaView>
     );
 }
